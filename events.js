2b85c67345d46771c76f2e267a9c8401e050c41d
David Guttman <david@davidguttman.com>
2022-05-24 14:38:23 -0700

    no db

--- events.js
+++ events.js
@@ -1,22 +1,22 @@
 const { EventEmitter } = require('events')
 
-const db = require('./db')
-const pub = db()
-const sub = db()
+// const db = require('./db')
+// const pub = db()
+// const sub = db()
 
-const PUBSUB_KEY = 'events'
+// const PUBSUB_KEY = 'events'
 
 const signalEmitter = new EventEmitter()
 
-sub.on('message', function (channel, data) {
-  try {
-    const { key, message } = JSON.parse(data)
-    return signalEmitter.emit(key, message)
-  } catch (err) {
-    return console.error(err)
-  }
-})
-sub.subscribe(PUBSUB_KEY)
+// sub.on('message', function (channel, data) {
+//   try {
+//     const { key, message } = JSON.parse(data)
+//     return signalEmitter.emit(key, message)
+//   } catch (err) {
+//     return console.error(err)
+//   }
+// })
+// sub.subscribe(PUBSUB_KEY)
 
 module.exports = {
   signal,
@@ -24,8 +24,9 @@ module.exports = {
 }
 
 function signal ({ key, message }) {
-  pub.publish(PUBSUB_KEY, JSON.stringify({
-    key,
-    message
-  }))
+  signalEmitter.emit(key, message)
+  // pub.publish(PUBSUB_KEY, JSON.stringify({
+  //   key,
+  //   message
+  // }))
 }
