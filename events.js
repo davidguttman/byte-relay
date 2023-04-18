ffb5aa2683feb67f7961430a2a1df59bbc8e7027
David Guttman <david@davidguttman.com>
2023-04-18 08:27:08 -0700

    refactor: remove unused db

--- events.js
+++ events.js
@@ -1,22 +1,6 @@
 const { EventEmitter } = require('events')
 
-// const db = require('./db')
-// const pub = db()
-// const sub = db()
-
-// const PUBSUB_KEY = 'events'
-
 const signalEmitter = new EventEmitter()
-
-// sub.on('message', function (channel, data) {
-//   try {
-//     const { key, message } = JSON.parse(data)
-//     return signalEmitter.emit(key, message)
-//   } catch (err) {
-//     return console.error(err)
-//   }
-// })
-// sub.subscribe(PUBSUB_KEY)
 
 module.exports = {
   signal,
@@ -25,8 +9,4 @@ module.exports = {
 
 function signal ({ key, message }) {
   signalEmitter.emit(key, message)
-  // pub.publish(PUBSUB_KEY, JSON.stringify({
-  //   key,
-  //   message
-  // }))
 }
